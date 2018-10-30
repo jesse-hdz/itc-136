47e6f26a1620a5c405e2e83668d9712715a50b3e
jesse-hdz <jessi.hernandez@seattlecolleges.edu>
2018-10-29 20:46:38 -0700

    added new commands to script

--- my_script.sh
+++ my_script.sh
@@ -3,3 +3,13 @@
 
 echo "hello ITC-136"
 echo "second echo statement"
+echo "this is you current working directory: "
+pwd
+
+echo "This is the version of linux you're running: "
+uname -a
+
+echo "These are your logged in users: "
+who
+
+
