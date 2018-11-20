86b4edada6fb420ed7e4b7a9a7fbb1e7fa238e5c
Jesse Hernandez <37102346+jesse-hdz@users.noreply.github.com>
2018-11-19 20:03:20 -0800

    Create stats.sh script

---
+++ stats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#how much disk space has been used
+diskused=$( df -h | grep "/dev/sda1" | awk '{print $5}' )
+
+#how much memory is free (available memory)
+freemem=$( free -mh | grep Mem: | awk '{print $7}' )
+
+#how many connections there are to the current machine
+connections=$( netstat | grep tcp )
+
+#who is logged in
+users=$( who )
+
+echo "This is a snapshot of your current system:"
+
+                Disk Used: $diskused
+                
+                Free Memory: $freemem
+                
+                Logged In Users: $users
+                
+Open Internet Connections:
+$connections
+"
+
+echo " 
+color info:
+http://misc.flogisoft.com/bash/tip_colors_formatting
+http://wiki.bash-hackers.org/scripting/style
+https://help.ubuntu.com/community/UnityLaunchersAndDesktopFiles
+"
