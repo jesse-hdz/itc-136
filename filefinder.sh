515f6fce3ad2d3f821486b3f036283044221d466
Jesse Hernandez <37102346+jesse-hdz@users.noreply.github.com>
2018-11-26 20:22:21 -0800

    update filefinder.sh
    update of user input

--- filefinder.sh
+++ filefinder.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 printf "\n Please enter a file name "
-read num3
-printf "\n Please enter the path to check "
-read path2check
+read fname
 
-if find $path2check -name $num3 -print -quit |
-   grep -q '^'; then
-  echo "the file exists!"
-else
-  echo "the file does not exist!"
-fi
+#find the file
+filefound=$( readlink -f $fname )
+
+#printf "\n Please enter the path to check "
+#read path2check
+
+
+echo -en "\e[94m Your File is here: $filefound"
+
+
+#if find $path2check -name $fname -print -quit |
+   #grep -q '^'; then
+  #echo "the file exists!"
+#else
+  #echo "the file does not exist!"
+#fi
