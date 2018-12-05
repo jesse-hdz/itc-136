4e6464de7ed21b7855a1c41289a44bf2b1a18b93
Jesse Hernandez <37102346+jesse-hdz@users.noreply.github.com>
2018-12-05 00:26:02 -0800

    Update filefinder.sh
    updated script using conditionals for output

--- filefinder.sh
+++ filefinder.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
-printf "\n Please enter a file name: "
+#Where did I put that file?: Write a script that utilizes locate, find, 
+#whereis and which to determine the location of a lost file and whether 
+#or not it is in the PATH, if it is an executable file.
+
+printf "\n Please enter a file name to locate: "
 read fname
 
-#find the file
-filefound=$( readlink -f $fname )
+#find the file ----------------------------------------------------------
+#[ ! -f "$fname" ]
+#filefound=$( find . -name $fname )
+#filefound=$( readlink -f $fname )
 
-#locate the file (sudo updatedb)
-filelocated=$( locate *$fname* )
+#finds files regardless of case "-iname"
+if [[ find / -iname "$fname" ]] 
+then
+    echo "$0: File '${fname}' is located at: $filelocated"
+    echo "$0: '${fname}' is : $filewhereis"
+         if [[ -x "$fname" ]] #$filewhich
+         then
+             echo "$0: File '$fname' is executable."
+         else
+             echo "$0: File '$fname' is not executable or found."
+         fi
+else
+    echo "$0: File '${fname}' not found."
+fi
 
-#whereis the file
+#locate the file --------------------------------------------------------
+filelocated=$( locate $fname )
+
+#whereis the file -------------------------------------------------------
 filewhereis=$( whereis $fname )
 
+#which (executable file) ------------------------------------------------
+filewhich=$( which -a $fname )
+
 #executable?
-executablefile=$( ls -l $fname )
-
-#printf "\n Please enter the path to check "
-#read path2check
-
+#fileexecutable=$( -x "$fname" )
 
 echo -en "\e[94m Your File: $executablefile
  Found: $filefound
